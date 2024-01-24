e149d6ec2957b0074ea118fc6847f3d4431ddb67
Eloy Parga <eloypargadev@gmail.com>
2024-01-24 15:14:11 +0100

    add Components
    Se crean los siguientes componentes:
    -Geetings.svelte
    -PlaylistItemCard
    -DideMenuCard
    
    Se crea la animación desde el index al [id].astro de tal forma que se hace de forma dinámica vinculando la id del album musical

---
+++ src/components/Greetings.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+
+    let greeting =""
+
+    if(currentHour<12 && currentHour>6){
+        greeting="Buenos días"
+    }else if(currentHour <20){
+        greeting = "Buenas tardes"
+    }else{
+        greeting = "Buenas Noches"
+    }
+</script>
+
+<h1 class="text-4xl font-bold">{greeting}</h1>
